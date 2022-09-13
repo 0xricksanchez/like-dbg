37140186cfbf300d6504b32c78a5e44dc686f463
434b <17012133+0xricksanchez@users.noreply.github.com>
2022-09-13 10:20:26 +0200

    Update misc.py

--- src/misc.py
+++ src/misc.py
@@ -26,7 +26,7 @@ def cfg_setter(obj, sections: list[str], user_cfg: str, exclude_keys: list[str] 
     if user_cfg and Path(user_cfg).exists():
         CFGS.append(Path(user_cfg).absolute())
     for c in CFGS:
-        if user_cfg == str(c):
+        if Path(user_cfg).name == c.name:
             ignore_empty = True
         cfg.read(c)
         _set_base_cfg(cfg, exclude_keys, obj, sections, ignore_empty)
