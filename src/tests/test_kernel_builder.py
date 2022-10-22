e49198deb93301859cdc21ff33bb3c0a5b1a1904
--global <admin@0x434b.dev>
2022-10-22 12:14:07 +0200

    .

--- src/tests/test_kernel_builder.py
+++ src/tests/test_kernel_builder.py
@@ -1,16 +1,18 @@
-from ..kernel_builder import KernelBuilder
+from ..kernel_builder import KernelBuilder, MISC_DRVS_PATH
 from pathlib import Path
 import collections
 import configparser
 from unittest.mock import patch
+import uuid
 
 
 USER_INI = Path("configs/user.ini")
+CUSTOM_MODULE = Path("examples/like_dbg_confs/echo_module.ini")
 
 
-def fetch_cfg_value_from_section_and_key(sect: str, key: str) -> str:
+def fetch_cfg_value_from_section_and_key(c: Path, sect: str, key: str) -> str:
     cfg = configparser.ConfigParser()
-    cfg.read(USER_INI)
+    cfg.read(c)
     return cfg[sect][key]
 
 
@@ -49,14 +51,14 @@ def test_extra_args() -> None:
 def test_get_params_syzkaller(self) -> None:
     kb = KernelBuilder(**{"kroot": "foo"})
     kb.mode = "syzkaller"
-    assert kb._get_params() == fetch_cfg_value_from_section_and_key("kernel_builder", "syzkaller_args")
+    assert kb._get_params() == fetch_cfg_value_from_section_and_key(USER_INI, "kernel_builder", "syzkaller_args")
 
 
 @patch("src.kernel_builder.KernelBuilder._run_ssh", return_value=0)
 def test_get_params_generic(self) -> None:
     kb = KernelBuilder(**{"kroot": "foo"})
     kb.mode = "generic"
-    assert kb._get_params() == fetch_cfg_value_from_section_and_key("kernel_builder", "generic_args")
+    assert kb._get_params() == fetch_cfg_value_from_section_and_key(USER_INI, "kernel_builder", "generic_args")
 
 
 @patch("src.kernel_builder.KernelBuilder._run_ssh", return_value=0)
@@ -81,4 +83,29 @@ def test_get_params_extra_override(self) -> None:
     kb = KernelBuilder(**{"kroot": "foo"})
     kb.mode = "generic"
     kb.extra_args = "-d DEBUG_KERNEL"
-    assert kb._get_params() == fetch_cfg_value_from_section_and_key("kernel_builder", "generic_args").replace("-e DEBUG_KERNEL", kb.extra_args)
+    assert kb._get_params() == fetch_cfg_value_from_section_and_key(USER_INI, "kernel_builder", "generic_args").replace("-e DEBUG_KERNEL", kb.extra_args)
+
+def test_add_modules() -> None:
+    kb = KernelBuilder(**{"kroot": "foo"})
+    kb.custom_modules = fetch_cfg_value_from_section_and_key(CUSTOM_MODULE, "kernel_builder", "custom_modules")
+    p = Path(f"/tmp/{uuid.uuid1().hex}")
+    Path(p / MISC_DRVS_PATH).mkdir(parents=True)
+    fst = "This is the 1st line.\n"
+    lst = "This is the last line.\n"
+    q = Path(p / MISC_DRVS_PATH / "Makefile")
+    q.touch()
+    q.write_text(fst)
+    r = Path(p / MISC_DRVS_PATH / "Kconfig")
+    r.touch()
+    r.write_text(f"{fst}\n{lst}!")
+    kb._add_modules()
+    with open(q, "r") as f:
+        data = f.readlines()
+    assert data[-1] != fst
+    with open(r, "r") as f:
+        data = f.readlines()
+    assert data[-1] == lst
+    assert data[-2] != fst 
+
+
+
